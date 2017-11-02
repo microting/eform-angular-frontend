d26578ad8cb88407a0d0c2a72fd419d065bb52a6
Yehor <gid733@ya.ru>
2017-11-02 11:30:27 +0200

    Fix inf loop

--- eform-client/src/app/components/header/header.component.ts
+++ eform-client/src/app/components/header/header.component.ts
@@ -15,16 +15,28 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.settingsService.getHeaderSettings().subscribe((data => {
-      if (data && data.success) {
-        this.headerSettingsModel = data.model;
-        if (this.headerSettingsModel.imageLink && this.headerSettingsModel.imageLinkVisible) {
-          this.logoImage = 'api/images/eform-images?fileName=' + this.headerSettingsModel.imageLink;
-        } else if (!this.headerSettingsModel.imageLink) {
-          this.logoImage = '../../../assets/images/logo.png';
-        }
+    this.settingsService.connectionStringExist().subscribe((result) => {
+      if (result && result.success === true) {
+        this.settingsService.getHeaderSettings().subscribe((data => {
+          if (data && data.success) {
+            this.headerSettingsModel = data.model;
+            if (this.headerSettingsModel.imageLink && this.headerSettingsModel.imageLinkVisible) {
+              this.logoImage = 'api/images/eform-images?fileName=' + this.headerSettingsModel.imageLink;
+            } else if (!this.headerSettingsModel.imageLink) {
+              this.logoImage = '../../../assets/images/logo.png';
+            }
+          }
+        }));
+      } else {
+        this.logoImage = '../../../assets/images/logo.png';
+        this.headerSettingsModel.imageLinkVisible = true;
+        this.headerSettingsModel.mainTextVisible = true;
+        this.headerSettingsModel.secondaryTextVisible = true;
+        this.headerSettingsModel.mainText = 'Microting eForm';
+        this.headerSettingsModel.secondaryText = 'No more paper-forms and back-office data entry';
       }
-    }));
+    });
+
   }
 
 }
