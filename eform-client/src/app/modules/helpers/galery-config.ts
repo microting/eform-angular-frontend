f55aec4626153d1dc149c19d58269a70ca3f9ffc
Gid <gid733@ya.ru>
2017-11-17 09:33:47 +0200

    Gallery & anchors fix

--- eform-client/src/app/modules/helpers/galery-config.ts
+++ eform-client/src/app/modules/helpers/galery-config.ts
@@ -1,23 +1,46 @@
 import {GalleryConfig} from 'ng-gallery';
 
-export const galleryConfig: GalleryConfig = {
+export const galleryConfig: GalleryConfig =  {
+  'gestures': true,
   'style': {
     'background': '#121519',
     'width': '900px',
     'height': '600px'
   },
-  'animation': 'fade',
-  'loader': false,
+  'navigation': true,
+  'loader': {
+    'width': '50px',
+    'height': '50px',
+    'icon': 'oval',
+//    'position': 'center'
+  },
   'description': {
     'position': 'bottom',
     'overlay': true,
     'text': true,
-    'counter': true
+    'counter': true,
+    'style': {
+      'textShadow': '0 0 2px rgba(0,0,0,0.5)'
+    }
   },
-  'bullets': {},
-  'player': false,
-  'thumbnails': false,
-  'navigation': true,
-  'gestures': false
+  'player': {
+    'autoplay': false,
+    'interval': 3000,
+    'progress': true,
+   // 'speed': 3000
+  },
+  'thumbnails': {
+    'width': 70,
+    'height': 70,
+    'position': 'top',
+    'space': 10
+  },
+  'lightbox': {
+    'backdropClass': 'g-backdrop',
+    'panelClass': 'g-overlay',
+    'hasBackdrop': true,
+    'positionStrategy': 'GlobalPositionStrategy',
+    'scrollStrategy': 'BlockScrollStrategy'
+  },
+  'bullets': false
 };
-
