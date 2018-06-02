bc007f4eff982778e80c60258100781b37fdf6d4
Leonid <em-atf@mail.ru>
2018-06-02 13:24:37 +0300

    Added Platform target for base plugin

--- eFormAPI/Plugins/eFormBase/EformBase.Pn/Infrastructure/EFormCoreHelper.cs
+++ eFormAPI/Plugins/eFormBase/EformBase.Pn/Infrastructure/EFormCoreHelper.cs
@@ -37,19 +37,10 @@ namespace EformBase.Pn.Infrastructure
             }
 
 
-            string connectionStr = lines.First();
+            var connectionStr = lines.First();
 
             _core = new Core();
-            bool running = false;
-            _core.HandleCaseCreated += EventCaseCreated;
-            _core.HandleCaseRetrived += EventCaseRetrived;
-            _core.HandleCaseCompleted += EventCaseCompleted;
-            _core.HandleCaseDeleted += EventCaseDeleted;
-            _core.HandleFileDownloaded += EventFileDownloaded;
-            _core.HandleSiteActivated += EventSiteActivated;
-            //_core.HandleEventLog += EventLog;
-            //_core.HandleEventMessage += EventMessage;
-            //_core.HandleEventWarning += EventWarning;
+            var running = false;
             _core.HandleEventException += EventException;
 
             try
@@ -68,99 +59,15 @@ namespace EformBase.Pn.Infrastructure
             {
                 container = new WindsorContainer();
                 container.Register(Component.For<Core>().Instance(_core));
-                container.Install(
-                        new RebusHandlerInstaller()
-                        , new RebusInstaller(System.Configuration.ConfigurationManager.ConnectionStrings["eFormMainConnection"].ConnectionString)
-                    );
+                container.Install(new RebusHandlerInstaller(), new RebusInstaller(System.Configuration.ConfigurationManager.ConnectionStrings["eFormMainConnection"].ConnectionString));
                 this.bus = container.Resolve<IBus>();
                 return _core;
             }
             Logger.Error("Core is not running");
             throw new Exception("Core is not running");
-            //return null;
         }
 
         #region events
-
-        public void EventCaseCreated(object sender, EventArgs args)
-        {
-            // Does nothing for web implementation
-        }
-
-        public void EventCaseRetrived(object sender, EventArgs args)
-        {
-            // Does nothing for web implementation
-        }
-
-        public void EventCaseCompleted(object sender, EventArgs args)
-        {
-            // Does nothing for web implementation
-        }
-
-        public void EventCaseDeleted(object sender, EventArgs args)
-        {
-            // Does nothing for web implementation
-        }
-
-        public void EventFileDownloaded(object sender, EventArgs args)
-        {
-            // Does nothing for web implementation
-        }
-
-        public void EventSiteActivated(object sender, EventArgs args)
-        {
-            // Does nothing for web implementation
-        }
-
-        public void EventLog(object sender, EventArgs args)
-        {
-            try
-            {
-                Logger.Trace(sender + Environment.NewLine);
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("bin") && ex.Message.Contains("log.txt"))
-                {
-                    System.IO.Directory.CreateDirectory(System.Web.Hosting.HostingEnvironment.MapPath("~/bin/log"));
-                }
-                EventException(ex, EventArgs.Empty);
-            }
-        }
-
-        public void EventMessage(object sender, EventArgs args)
-        {
-            try
-            {
-                Logger.Trace(sender + Environment.NewLine);
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("bin") && ex.Message.Contains("log.txt"))
-                {
-                    System.IO.Directory.CreateDirectory(
-                        System.Web.Hosting.HostingEnvironment.MapPath("~/bin/log/log.txt"));
-                }
-                EventException(ex, EventArgs.Empty);
-            }
-        }
-
-        public void EventWarning(object sender, EventArgs args)
-        {
-            try
-            {
-                Logger.Trace("## WARNING ## " + sender + " ## WARNING ## " + Environment.NewLine);
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("bin") && ex.Message.Contains("log.txt"))
-                {
-                    System.IO.Directory.CreateDirectory(
-                        System.Web.Hosting.HostingEnvironment.MapPath("~/bin/log/log.txt"));
-                }
-                EventException(ex, EventArgs.Empty);
-            }
-        }
 
         public void EventException(object sender, EventArgs args)
         {
