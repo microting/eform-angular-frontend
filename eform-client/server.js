4b9451d013131d653c4b2c1bc1b30f57a20d274c
René Schultz Madsen <rm@microting.com>
2018-06-11 15:37:40 +0200

    Trying with allowing the changeOrigin.

--- eform-client/server.js
+++ eform-client/server.js
@@ -6,7 +6,7 @@ const app = express();
 const defaultPort = process.env.PORT || $$port$$;
 
 // proxy
-var apiProxy = httpProxy.createProxyServer();
+var apiProxy = httpProxy.createProxyServer({"changeOrigin": true});
 var apiForwardingUrl = 'http://localhost:$$apiport$$/';
 apiProxy.on('error', function(e) {
   console.error('Error:');
