bc581efb68e03a0c786582dbbe2d59b1678ce0ec
Leonid <em-atf@mail.ru>
2018-07-05 22:37:56 +0300

    Add AudioController

---
+++ eFormAPI/eFormAPI/Controllers/AudioController.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace eFormAPI.Web.Controllers
+{
+    [Authorize]
+    public class AudioController : ApiController
+    {
+        [HttpGet]
+        [Route("api/audio/eform-audio/{fileName}")]
+        public HttpResponseMessage GetAudio(string fileName)
+        {
+            var filePath = HttpContext.Current.Server.MapPath($"~/output/datafolder/picture/{fileName}.wav");
+            if (!File.Exists(filePath))
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+            const string mime = "vnd.wave";
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StreamContent(fileStream)
+            };
+            result.Content.Headers.ContentDisposition =
+                new ContentDispositionHeaderValue("attachment") {FileName = fileName};
+            result.Content.Headers.ContentType =
+                new MediaTypeHeaderValue($"audio/{mime}");
+            return result;
+        }
+    }
+}
