3bc43d8c3bc766bec5a679d4adb6a037845a5b71
René Schultz Madsen <rm@microting.com>
2018-10-19 12:00:03 +0200

    Adding changes to give back the correct id.

--- eFormAPI/eFormAPI.Web/Services/EntitySelectService.cs
+++ eFormAPI/eFormAPI.Web/Services/EntitySelectService.cs
@@ -152,7 +152,7 @@ namespace eFormAPI.Web.Services
                 {
                     mappedEntityGroupDict.Add(new CommonDictionaryTextModel()
                     {
-                        Id = entityGroupItem.MicrotingUUID,
+                        Id = entityGroupItem.Id.ToString(),
                         Text = entityGroupItem.Name
                     });
                 }
