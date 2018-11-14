e48fe9f1bfbe79844ece902fa0bcc07f04a2f211
René Schultz Madsen <rm@microting.com>
2018-11-14 14:23:18 +0100

    Adding basic test structure.

---
+++ eFormAPI/eFormAPI.Web.Tests/CanaryInAColeMine.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace eFormSDK.Tests
+{
+    [TestFixture]
+    public class CanaryInAColeMine
+    {
+        [Test]
+        public void UnitCanPeep()
+        {
+            Assert.True(true);
+        }
+    }
+}
