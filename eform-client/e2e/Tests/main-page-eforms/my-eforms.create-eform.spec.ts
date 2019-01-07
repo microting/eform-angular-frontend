f2006f99881b8f3e624b971adba280a30efaa198
René Schultz Madsen <rm@microting.dk>
2019-01-07 07:57:15 +0100

    Adding back the first 3 tests.

--- eform-client/e2e/Tests/main-page-eforms/my-eforms.create-eform.spec.ts
+++ eform-client/e2e/Tests/main-page-eforms/my-eforms.create-eform.spec.ts
@@ -8,51 +8,51 @@ describe('My eforms', function () {
     loginPage.open('/');
     loginPage.login();
   });
-  // it('should create eform without any tags', function () {
-  //   const newEformLabel = Guid.create().toString();
-  //   myEformsPage.createNewEform(newEformLabel);
-  //   let eform = myEformsPage.getFirstMyEformsRowObj();
-  //   expect(eform.eFormName).equal(newEformLabel);
-  //   expect(eform.tags.length).equal(0);
-  //   eform.deleteBtn.click();
-  //   browser.pause(10000);
-  //   $$('.btn-danger')[2].click();
-  //   eform = myEformsPage.getFirstMyEformsRowObj();
-  //   expect(eform.id === null);
-  // });
-  // it('should create eform simultaneously with creating 1 tag', function () {
-  //   const newEformLabel = Guid.create().toString();
-  //   const createdTag = Guid.create().toString();
-  //   myEformsPage.createNewEform(newEformLabel, [createdTag]);
-  //   let eform = myEformsPage.getFirstMyEformsRowObj();
-  //   expect(eform.eFormName).equal(newEformLabel);
-  //   expect(eform.tags.length).equal(1);
-  //   expect(eform.tags[0].getText()).equal(createdTag);
-  //   eform.deleteBtn.click();
-  //   browser.pause(10000);
-  //   $$('.btn-danger')[2].click();
-  //   eform = myEformsPage.getFirstMyEformsRowObj();
-  //   expect(eform.id === null);
-  //
-  // });
-  // it('should create eform simultaneously with creating 2 tags', function () {
-  //   const newEformLabel = Guid.create().toString();
-  //   const createdTags = [Guid.create().toString(), Guid.create().toString()];
-  //   const addedAndSelectedTags = myEformsPage.createNewEform(newEformLabel, createdTags);
-  //   let eform = myEformsPage.getFirstMyEformsRowObj();
-  //   const tagsTexts = eform.tags.map(el => {
-  //     return el.getText();
-  //   });
-  //   expect(eform.eFormName).equal(newEformLabel);
-  //   expect(eform.tags.length).equal(createdTags.length);
-  //   expect(tagsTexts).to.include.members(createdTags);
-  //   eform.deleteBtn.click();
-  //   browser.pause(7000);
-  //   $$('.btn-danger')[2].click();
-  //   browser.pause(7000);
-  //   eform = myEformsPage.getFirstMyEformsRowObj();
-  //   expect(eform.id === null);
-  // });
+  it('should create eform without any tags', function () {
+    const newEformLabel = Guid.create().toString();
+    myEformsPage.createNewEform(newEformLabel);
+    let eform = myEformsPage.getFirstMyEformsRowObj();
+    expect(eform.eFormName).equal(newEformLabel);
+    expect(eform.tags.length).equal(0);
+    eform.deleteBtn.click();
+    browser.pause(10000);
+    $$('.btn-danger')[2].click();
+    eform = myEformsPage.getFirstMyEformsRowObj();
+    expect(eform.id === null);
+  });
+  it('should create eform simultaneously with creating 1 tag', function () {
+    const newEformLabel = Guid.create().toString();
+    const createdTag = Guid.create().toString();
+    myEformsPage.createNewEform(newEformLabel, [createdTag]);
+    let eform = myEformsPage.getFirstMyEformsRowObj();
+    expect(eform.eFormName).equal(newEformLabel);
+    expect(eform.tags.length).equal(1);
+    expect(eform.tags[0].getText()).equal(createdTag);
+    eform.deleteBtn.click();
+    browser.pause(10000);
+    $$('.btn-danger')[2].click();
+    eform = myEformsPage.getFirstMyEformsRowObj();
+    expect(eform.id === null);
+
+  });
+  it('should create eform simultaneously with creating 2 tags', function () {
+    const newEformLabel = Guid.create().toString();
+    const createdTags = [Guid.create().toString(), Guid.create().toString()];
+    const addedAndSelectedTags = myEformsPage.createNewEform(newEformLabel, createdTags);
+    let eform = myEformsPage.getFirstMyEformsRowObj();
+    const tagsTexts = eform.tags.map(el => {
+      return el.getText();
+    });
+    expect(eform.eFormName).equal(newEformLabel);
+    expect(eform.tags.length).equal(createdTags.length);
+    expect(tagsTexts).to.include.members(createdTags);
+    eform.deleteBtn.click();
+    browser.pause(7000);
+    $$('.btn-danger')[2].click();
+    browser.pause(7000);
+    eform = myEformsPage.getFirstMyEformsRowObj();
+    expect(eform.id === null);
+  });
   it('should create eform with creating 1 tag and using 1 already prepared tag', function () {
     let eform = myEformsPage.getFirstMyEformsRowObj();
     expect(eform.id === null);
