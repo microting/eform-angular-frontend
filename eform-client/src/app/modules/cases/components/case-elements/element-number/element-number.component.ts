5299094ef321fc282d1667c230443af287de6945
Gid <gid733@ya.ru>
2019-01-22 11:16:09 +0200

    Numbers fix

--- eform-client/src/app/modules/cases/components/case-elements/element-number/element-number.component.ts
+++ eform-client/src/app/modules/cases/components/case-elements/element-number/element-number.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {FieldValueDto} from 'src/app/common/models';
 
 @Component({
@@ -6,21 +7,28 @@ import {FieldValueDto} from 'src/app/common/models';
   templateUrl: './element-number.component.html',
   styleUrls: ['./element-number.component.scss']
 })
-export class ElementNumberComponent  {
+export class ElementNumberComponent implements OnInit {
   fieldValueObj: FieldValueDto = new FieldValueDto();
-
+  numberForm: FormGroup;
   @Input()
   get fieldValue() {
+    this.fieldValueObj.value = this.numberForm.getRawValue().numberControl;
     return this.fieldValueObj;
   }
 
   set fieldValue(val) {
     this.fieldValueObj = val;
+    this.numberForm.setValue({numberControl: val.value});
   }
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+
   }
 
   ngOnInit() {
+    this.numberForm = this.formBuilder.group({
+      numberControl: ['', Validators.required,
+        Validators.pattern('^[1-9][\\.\\d]*(,\\d+)?$')]
+    });
   }
 }
