9455068bb19669ef92e873910b4bf602ccda26b1
René Schultz Madsen <rm@microting.dk>
2019-01-24 08:47:57 +0100

    Removing the plugins.

--- eform-client/src/app/plugins/plugins.routing.ts
+++ eform-client/src/app/plugins/plugins.routing.ts
@@ -3,21 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from 'src/app/common/guards';
 
 export const routes: Routes = [
-  {
-    path: "customers-pn",
-    canActivate: [AuthGuard],
-    loadChildren: "./modules/customers-pn/customers-pn.module#CustomersPnModule"
-  },
-  {
-    path: "case-management-pn",
-    canActivate: [AuthGuard],
-    loadChildren: "./modules/case-management-pn/case-management-pn.module#CaseManagementPnModule"
-  },
-  {
-    path: "machine-area-pn",
-    canActivate: [AuthGuard],
-    loadChildren: "./modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule"
-  }
   // INSERT ROUTES HERE
 ];
 
