4aeef39534901024807a69124cb30da6a963fe36
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-01-30 09:27:42 +0100

    Adding test for Name Changing, password change and can assert language change.

---
+++ eform-client/e2e/Page objects/UserAdministration.page.ts
@@ -0,0 +1,46 @@
+import {PageWithNavbarPage} from './PageWithNavbar.page';
+
+export class UserAdministration extends  PageWithNavbarPage {
+  constructor() {
+    super();
+  }
+    public get editUserBtn() {
+    return browser.element('#userAdministrationEditBtn');
+    }
+    public get firstNameBox() {
+    return browser.element('#editFirstName');
+    }
+    public get lastNameBox() {
+    return browser.element('#editLastName');
+    }
+    public get saveBtn() {
+    return browser.element('#editUserSaveBtn');
+    }
+    public setNewName(firstName: string, lastName: string) {
+    this.editUserBtn.click();
+    browser.pause(2000);
+    this.firstNameBox.clearElement();
+    browser.pause(500);
+    this.firstNameBox.addValue(firstName);
+    this.lastNameBox.clearElement();
+    browser.pause(500);
+    this.lastNameBox.addValue(lastName);
+    browser.pause(500);
+    this.saveBtn.click();
+    }
+    public revertToOldName(firstName: string, lastName: string) {
+      this.editUserBtn.click();
+      browser.pause(2000);
+      this.firstNameBox.clearElement();
+      browser.pause(500);
+      this.firstNameBox.addValue(firstName);
+      this.lastNameBox.clearElement();
+      browser.pause(500);
+      this.lastNameBox.addValue(lastName);
+      browser.pause(500);
+      this.saveBtn.click();
+    }
+}
+
+const userAdministration = new UserAdministration();
+export default userAdministration;
--- eform-client/e2e/Tests/profile-settings/profile-settings.language.spec.ts
+++ eform-client/e2e/Tests/profile-settings/profile-settings.language.spec.ts
@@ -14,24 +14,24 @@ describe('Profile Settings', function () {
     profileSettings.chooseLanguage('English');
     profileSettings.saveProfileSettings();
     browser.refresh();
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('My eForms')).equal('My eForms');
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Device Users')).equal('Device Users');
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Advanced')).equal('Advanced');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('My eForms')).equal('My eForms');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Device Users')).equal('Device Users');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Advanced')).equal('Advanced');
   });
   it('should set language to German', function () {
     profileSettings.chooseLanguage('German');
     profileSettings.saveProfileSettings();
     browser.refresh();
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Meine eForms')).equal('Meine eForms');
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Gerätebenutzer')).equal('Gerätebenutzer');
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Fortgeschritten')).equal('Fortgeschritten');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Meine eForms')).equal('Meine eForms');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Gerätebenutzer')).equal('Gerätebenutzer');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Fortgeschritten')).equal('Fortgeschritten');
   });
   it('should set language to Danish', function () {
     profileSettings.chooseLanguage('Danish');
     profileSettings.saveProfileSettings();
     browser.refresh();
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Mine eForms')).equal('Mine eForms');
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Enhedsbrugere')).equal('Enhedsbrugere');
-    // expect(myEformsPage.Navbar.verifyHeaderMenuItem('Avanceret')).equal('Avanceret');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Mine eForms')).equal('Mine eForms');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Enhedsbrugere')).equal('Enhedsbrugere');
+    expect(myEformsPage.Navbar.verifyHeaderMenuItem('Avanceret')).equal('Avanceret');
   });
 });
