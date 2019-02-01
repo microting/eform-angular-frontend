1440aca7bdf5460a5c2b37d6c53e598b778830fc
René Schultz Madsen <rm@microting.dk>
2019-02-01 14:18:27 +0100

    Removing the CaseDelete("123")

--- eFormAPI/eFormAPI.Web/Services/TemplatesService.cs
+++ eFormAPI/eFormAPI.Web/Services/TemplatesService.cs
@@ -308,7 +308,6 @@ namespace eFormAPI.Web.Services
             foreach (var siteUId in sitesToBeRetractedFrom)
             {
                 core.CaseDelete(deployModel.Id, siteUId);
-                core.CaseDelete("123");
             }
 
             return new OperationResult(true,
