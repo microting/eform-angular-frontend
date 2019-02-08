f5e0e41a2f5c92f5fdf1cb12a90027122913f5ed
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-02-08 12:46:42 +0100

    adding worker tests.

--- eform-client/e2e/Tests/workers/workers.add.spec.ts
+++ eform-client/e2e/Tests/workers/workers.add.spec.ts
@@ -1,18 +1,56 @@
 import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
 import workers from '../../Page objects/Workers.page';
+import deviceUsersPage from '../../Page objects/DeviceUsers.page';
 
-describe('Workers page should add new Worker', function () {
+const expect = require('chai').expect;
+
+describe('Workers page ', function () {
   before(function () {
     loginPage.open('/');
     loginPage.login();
+    myEformsPage.Navbar.goToDeviceUsersPage();
+    deviceUsersPage.createNewDeviceUser('Gurkemine', 'Ralphine');
+    browser.pause(3000);
     myEformsPage.Navbar.goToWorkers();
     browser.pause(8000);
   });
-  it('with first and last name', function () {
+  it('should add new Worker with first and last name', function () {
     const name = 'Monty';
     const surName = 'Python';
-    workers.createNewWorker(name,  surName);
-
+    workers.createNewWorker(name, surName);
+    const newWorker = workers.getWorker(workers.rowNum);
+    expect(newWorker.firstName).equal(name);
+    expect(newWorker.lastName).equal(surName);
   });
+  it('should add new Worker with special character', function () {
+    const name = 'René';
+    const surName = 'Éhl©µ';
+    workers.createNewWorker(name, surName);
+    const newWorker = workers.getWorker(workers.rowNum + 1);
+    expect(newWorker.firstName).equal(name);
+    expect(newWorker.lastName).equal(surName);
+  });
+  // TODO
+  // it('should NOT add new worker with first name only', function () {
+  //   const name = 'Anders';
+  //   workers.createNewWorker(name, '');
+  //   const newWorker = workers.getWorker(workers.rowNum);
+  //   expect(newWorker.firstName).equal(name);
+  // });
+  // it('should NOT add new worker with last name only', function () {
+  //   const surName = 'Kragh';
+  //   workers.createNewWorker('' , surName);
+  //   const newWorker = workers.getWorker(workers.rowNum);
+  //   expect(newWorker.lastName).equal(surName);
+  // });
+  // it('should NOT add new worker with no name', function () {
+  //   const name = '';
+  //   const surName = '';
+  //   workers.createNewWorker(name , surName);
+  //   const newWorker = workers.getWorker(workers.rowNum);
+  //   expect(newWorker.firstName).equal('');
+  //   expect(newWorker.lastName).equal('');
+  // });
+
 });
--- eform-client/src/app/plugins/plugins.routing.ts
+++ eform-client/src/app/plugins/plugins.routing.ts
@@ -3,8 +3,21 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from 'src/app/common/guards';
 
 export const routes: Routes = [
-  // INSERT ROUTES HERE
-];
+  {
+    path: 'trash-inspection-pn',
+    canActivate: [AuthGuard],
+    loadChildren: './modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule'
+  },
+  {
+    path: 'customers-pn',
+    canActivate: [AuthGuard],
+    loadChildren: './modules/customers-pn/customers-pn.module#CustomersPnModule'
+  },
+  {
+    path: 'machine-area-pn',
+    canActivate: [AuthGuard],
+    loadChildren: './modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule'
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
