e88e8979dc57b3b1c244ac0ac720544abeb4b9b1
René Schultz Madsen <rm@microting.dk>
2019-02-08 13:12:26 +0100

    Fixing the broken routing file.

--- eform-client/src/app/plugins/plugins.routing.ts
+++ eform-client/src/app/plugins/plugins.routing.ts
@@ -3,21 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from 'src/app/common/guards';
 
 export const routes: Routes = [
-  {
-    path: 'trash-inspection-pn',
-    canActivate: [AuthGuard],
-    loadChildren: './modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule'
-  },
-  {
-    path: 'customers-pn',
-    canActivate: [AuthGuard],
-    loadChildren: './modules/customers-pn/customers-pn.module#CustomersPnModule'
-  },
-  {
-    path: 'machine-area-pn',
-    canActivate: [AuthGuard],
-    loadChildren: './modules/machine-area-pn/machine-area-pn.module#MachineAreaPnModule'
-  }];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
