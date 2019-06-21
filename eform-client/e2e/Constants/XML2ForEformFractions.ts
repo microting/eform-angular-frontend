f4233a9281d68e3d0be8e276ddc4bd54070a2a13
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-06-21 10:12:28 +0200

    adding xml for 2. eForm.

---
+++ eform-client/e2e/Constants/XML2ForEformFractions.ts
@@ -0,0 +1,44 @@
+export default {
+  XML: '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<Main>\n' +
+    '  <Id>138798</Id>\n' +
+    '  <Repeated>0</Repeated>\n' +
+    '  <Label>Number 1</Label>\n' +
+    '  <StartDate>2018-05-08</StartDate>\n' +
+    '  <EndDate>2028-05-08</EndDate>\n' +
+    '  <Language>da</Language>\n' +
+    '  <MultiApproval>false</MultiApproval>\n' +
+    '  <FastNavigation>false</FastNavigation>\n' +
+    '  <Review>false</Review>\n' +
+    '  <Summary>false</Summary>\n' +
+    '  <DisplayOrder>0</DisplayOrder>\n' +
+    '  <ElementList>\n' +
+    '    <Element type="DataElement">\n' +
+    '      <Id>138798</Id>\n' +
+    '      <Label>Number 1</Label>\n' +
+    '      <Description><![CDATA[]]></Description>\n' +
+    '      <DisplayOrder>0</DisplayOrder>\n' +
+    '      <ReviewEnabled>false</ReviewEnabled>\n' +
+    '      <ManualSync>false</ManualSync>\n' +
+    '      <ExtraFieldsEnabled>false</ExtraFieldsEnabled>\n' +
+    '      <DoneButtonDisabled>false</DoneButtonDisabled>\n' +
+    '      <ApprovalEnabled>false</ApprovalEnabled>\n' +
+    '      <DataItemList>\n' +
+    '        <DataItem type="Number">\n' +
+    '          <Id>343963</Id>\n' +
+    '          <Label>Number 1</Label>\n' +
+    '          <Description><![CDATA[Number 1 description]]></Description>\n' +
+    '          <DisplayOrder>0</DisplayOrder>\n' +
+    '          <Mandatory>true</Mandatory>\n' +
+    '          <MinValue>1</MinValue>\n' +
+    '          <MaxValue>1100</MaxValue>\n' +
+    '          <Value>24</Value>\n' +
+    '          <DecimalCount>2</DecimalCount>\n' +
+    '          <UnitName/>\n' +
+    '          <Color>e8eaf6</Color>\n' +
+    '        </DataItem>\n' +
+    '      </DataItemList>\n' +
+    '    </Element>\n' +
+    '  </ElementList>\n' +
+    '</Main>'
+};
