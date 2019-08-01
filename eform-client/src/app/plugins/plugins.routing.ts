b1ef8dda4bfd336e48e18494bd6894b4e668e6d6
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-08-01 13:36:48 +0200

    removing trash inspection from project.

--- eform-client/src/app/plugins/plugins.routing.ts
+++ eform-client/src/app/plugins/plugins.routing.ts
@@ -3,15 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from 'src/app/common/guards';
 
 export const routes: Routes = [
-  {
-
-    path: 'trash-inspection-pn',
-
-    canActivate: [AuthGuard],
-
-    loadChildren: './modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule'
-
-  }
+//INSERT ROUTS HERE
 ];
 
 @NgModule({
