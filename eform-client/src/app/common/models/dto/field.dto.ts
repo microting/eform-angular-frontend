1e10423d82cb0f6f885300b2bd5a7e8807d3b773
René Schultz Madsen <rm@microting.dk>
2019-09-19 07:15:30 +0200

    Fixing the Property 'description' in type 'FieldDto' is not assignable to the same property in base type 'DataItemDto'.   Type 'string' is not assignable to type 'Description'..

--- eform-client/src/app/common/models/dto/field.dto.ts
+++ eform-client/src/app/common/models/dto/field.dto.ts
@@ -1,10 +1,10 @@
-import {DataItemDto} from './data-item.dto';
+import {DataItemDto, Description} from './data-item.dto';
 import {KeyValuePairDto} from './key-value-pair.dto';
 
 export class FieldDto extends DataItemDto {
   id: number;
   label: string;
-  description: string;
+  description: Description;
   fieldTypeId: number;
   fieldType: string;
   checkListId: number;
