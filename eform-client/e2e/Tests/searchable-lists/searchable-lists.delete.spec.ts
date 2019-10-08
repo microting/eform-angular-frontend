8a38f3b14f47f6be64e8235d078b8acd0f68deff
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-10-08 08:02:15 +0200

    Adding more tests for entity search delete tests.

--- eform-client/e2e/Tests/searchable-lists/searchable-lists.delete.spec.ts
+++ eform-client/e2e/Tests/searchable-lists/searchable-lists.delete.spec.ts
@@ -4,3 +4,59 @@ import searchableLists from '../../Page objects/SearchableLists.page';
 
 const expect = require('chai').expect;
 
+describe('Entity Search', function () {
+  before(function () {
+    loginPage.open('/auth');
+    loginPage.login();
+  });
+  it('should go to entity search page', function () {
+    searchableLists.goToEntitySearchPage();
+    browser.waitForVisible('#createEntitySearchBtn', 20000);
+  });
+  it('should create a new searchable list with only name', function () {
+    const name = Guid.create().toString();
+    searchableLists.createSearchableList_NoItem(name);
+    const searchableList = searchableLists.getFirstRowObject();
+    expect(searchableList.name).equal(name);
+    browser.pause(8000);
+  });
+  it('should delete the list', function () {
+    searchableLists.deleteList();
+    expect(searchableLists.rowNum).equal(0);
+    browser.pause(8000);
+  });
+  it('should create a new searchable list with name and one item', function () {
+    const name = Guid.create().toString();
+    const itemName = Guid.create().toString();
+    searchableLists.createSearchableList_OneItem(name, itemName);
+    const searchableList = searchableLists.getFirstRowObject();
+    expect(searchableList.name).equal(name);
+    searchableList.editBtn.click();
+    browser.pause(4000);
+    expect(searchableLists.firstEntityItemName.getText()).equal(itemName);
+    searchableLists.entitySearchEditCancelBtn.click();
+    browser.pause(2000);
+  });
+  it('should delete item from list.', function () {
+    searchableLists.deleteItemFromList();
+    searchableLists.entitySearchEditBtn.click();
+    browser.waitForVisible('#editName', 20000);
+    expect(searchableLists.items).equal(0);
+    searchableLists.entitySearchEditCancelBtn.click();
+    searchableLists.cleanup();
+    browser.pause(8000);
+  });
+  it('should make a new searchable list with multiple items', function () {
+    const name = Guid.create().toString();
+    const itemNames = ['a \n', 'b\n', 'c\n', 'd\n', 'e'];
+    searchableLists.createSearchableList_MultipleItems(name, itemNames);
+    const searchableList = searchableLists.getFirstRowObject();
+    expect(searchableList.name).equal(name);
+    browser.pause(8000);
+  });
+  it('should delete a list with multiple items.', function () {
+    searchableLists.deleteList();
+    expect(searchableLists.rowNum).equal(0);
+    browser.pause(8000);
+  });
+});
