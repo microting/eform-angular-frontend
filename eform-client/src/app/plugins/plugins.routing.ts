dc1486e26e65b37e4d4b0998b4266fab90827ab1
René Schultz Madsen <rm@microting.dk>
2019-10-28 12:03:34 +0100

    Chaning the wait times a bit, to have a better success of starting testing.

--- eform-client/src/app/plugins/plugins.routing.ts
+++ eform-client/src/app/plugins/plugins.routing.ts
@@ -3,6 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from 'src/app/common/guards';
 
 export const routes: Routes = [
+{
+path: "trash-inspection-pn",
+canActivate: [AuthGuard],
+loadChildren: "./modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule"
+}
 // INSERT ROUTES HERE
 ];
 
