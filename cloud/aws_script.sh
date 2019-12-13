4b2c0b246ee5845e4432dfe37ba0d25597893a5a
René Schultz Madsen <rm@microting.com>
2019-12-13 11:53:18 +0100

    Adding more steps to the aws script.

--- cloud/aws_script.sh
+++ cloud/aws_script.sh
@@ -61,3 +61,77 @@ wget http://localhost:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
 
 ./rabbitmqadmin declare queue name=eformsdk-input durable=true
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+apt install -y apt-transport-https &&\
+apt update &&\
+apt install -y dotnet-runtime-2.2 dotnet-sdk-2.2
+
+chown -R ubuntu:ubuntu /var/www
+
+cd /var/www/microting
+
+echo "################## START CLONING ##################"
+su ubuntu -c \
+"git clone --progress --verbose https://github.com/microting/eform-angular-frontend.git -b stable"
+echo "################## END CLONING ##################"
+
+cd eform-angular-frontend/eform-client
+npm i -g @angular/cli
+su ubuntu -c \
+"npm i && npm run build"
+
+cd ../eFormAPI/eFormAPI.Web/
+echo "################## START GITVERSION ##################"
+export GITVERSION=`git describe --abbrev=0 --tags | cut -d "v" -f 2`
+echo $GITVERSION
+echo "################## END GITVERSION ##################"
+su ubuntu -c \
+"dotnet publish -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
+
+cat > /etc/systemd/system/eform.service << EndOfUnitFile
+[Unit]
+Description=eForm application
+[Service]
+WorkingDirectory=/var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out
+ExecStart=/usr/bin/dotnet /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/eFormAPI.Web.dll
+Restart=always
+RestartSec=10
+SyslogIdentifier=dotnet-eform-frontend
+User=ubuntu
+Environment=ASPNETCORE_ENVIRONMENT=Production
+[Install]
+WantedBy=multi-user.target
+EndOfUnitFile
+
+cat > /etc/nginx/sites-available/default << EndOfConfig
+server {
+    server_name _;
+    listen 80;
+    location / {
+        proxy_pass http://127.0.0.1:5000;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade \$http_upgrade;
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host \$host;
+        proxy_cache_bypass \$http_upgrad;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto \$scheme;
+	proxy_buffer_size   128k;
+	proxy_buffers   4 256k;
+	proxy_busy_buffers_size   256k;
+	fastcgi_buffers 16 16k;
+	fastcgi_buffer_size 32k;
+    }
+}
+EndOfConfig
+
+ln -s /var/www/microting/eform-angular-frontend/eform-client/dist /var/www/microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/wwwroot
+
+chown -R ubuntu:ubuntu /var/www
