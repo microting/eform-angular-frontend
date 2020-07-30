9eedf274dc25928b2be68e0e7856ff2f2b856c57
René Schultz Madsen <rm@microting.com>
2020-07-30 06:36:20 +0000

    Fixing the cases index to use the correct way of fetching timezone information from the currently logged in user.

--- eFormAPI/eFormAPI.Web/Services/CasesService.cs
+++ eFormAPI/eFormAPI.Web/Services/CasesService.cs
@@ -64,12 +64,22 @@ namespace eFormAPI.Web.Services
             try
             {
                 var value = _httpContextAccessor?.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier);
-                var local = _dbContext.Users.Single(x => x.Id == int.Parse(value)).Locale;
-                if (string.IsNullOrEmpty(local))
+                var timeZone = _dbContext.Users.Single(x => x.Id == int.Parse(value)).TimeZone;
+                if (string.IsNullOrEmpty(timeZone))
                 {
-                    local = "Europe/Copenhagen";
+                    timeZone = "Europe/Copenhagen";
                 }
-                TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(local);
+
+                TimeZoneInfo timeZoneInfo;
+
+                try
+                {
+                    timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+                }
+                catch
+                {
+                    timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time");
+                }
                 var core = await _coreHelper.GetCore();
                 var caseList = await core.CaseReadAll(requestModel.TemplateId, null, null,
                     Constants.WorkflowStates.NotRemoved, requestModel.NameFilter,
