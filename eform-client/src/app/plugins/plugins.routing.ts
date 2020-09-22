e16141fa0cad3f45385db7820c7f0fda4d6565bd
Gid <mkazli733@gmail.com>
2020-09-22 12:33:24 +0300

    Removed routings

--- eform-client/src/app/plugins/plugins.routing.ts
+++ eform-client/src/app/plugins/plugins.routing.ts
@@ -4,11 +4,6 @@ import {AuthGuard} from 'src/app/common/guards';
 
 export const routes: Routes = [
   // INSERT ROUTES HERE
-  {
-    path: 'work-orders-pn',
-    canActivate: [AuthGuard],
-    loadChildren: () => import('./modules/workorders-pn/work-orders-pn.module').then(m => m.WorkOrdersPnModule)
-  }
 ];
 
 @NgModule({
