930e4ce6d9acb1dcb827b3c82aa13ea4039d60df
Leonid <leonid.atf@gmail.com>
2020-10-06 17:26:39 +0300

    Added security group settings migration

--- eFormAPI/eFormAPI.Web/Migrations/20201006142508_AddSecurityGroupRedirectLink.Designer.cs
+++ eFormAPI/eFormAPI.Web/Migrations/20201006142508_AddSecurityGroupRedirectLink.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eFormAPI.Web.Infrastructure.Database;
 
 namespace eFormAPI.Web.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    partial class BaseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201006142508_AddSecurityGroupRedirectLink")]
+    partial class AddSecurityGroupRedirectLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
