fef240e04457309dd079263824eaeb30beb74102
Gid <mkazli733@gmail.com>
2020-11-30 19:36:30 +0200

    Added bulk import logic parts

--- eform-client/src/app/modules/eforms/components/eforms-bulk-import-modal/eforms-bulk-import-modal.component.ts
+++ eform-client/src/app/modules/eforms/components/eforms-bulk-import-modal/eforms-bulk-import-modal.component.ts
@@ -1,48 +1,67 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {FileUploader} from 'ng2-file-upload';
-import {ToastrService} from 'ngx-toastr';
-import {TranslateService} from '@ngx-translate/core';
-import {AuthService} from 'src/app/common/services';
-import {LoaderService} from 'src/app/common/services/loeader.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'src/app/common/services';
+import { LoaderService } from 'src/app/common/services/loeader.service';
 
 @Component({
   selector: 'app-eforms-bulk-import-modal',
   templateUrl: './eforms-bulk-import-modal.component.html',
-  styleUrls: ['./eforms-bulk-import-modal.component.scss']
+  styleUrls: ['./eforms-bulk-import-modal.component.scss'],
 })
 export class EformsBulkImportModalComponent implements OnInit {
-  @ViewChild('frame', {static: true}) frame;
+  @ViewChild('frame', { static: true }) frame;
+  @ViewChild('xlsxEforms', { static: false })
+  xlsxEformsInput: ElementRef;
   xlsxEformsFileUploader: FileUploader = new FileUploader({
-    url: '/api/templates/import', authToken: this.authService.bearerToken
+    url: '/api/templates/import',
+    authToken: this.authService.bearerToken,
   });
-  errors: {row: number, col: number, message: string}[];
+  errors: { row: number; col: number; message: string }[];
 
-  constructor(private toastrService: ToastrService,
-              private translateService: TranslateService,
-              private authService: AuthService,
-              private loaderService: LoaderService) {
-  }
+  constructor(
+    private toastrService: ToastrService,
+    private translateService: TranslateService,
+    private authService: AuthService,
+    private loaderService: LoaderService
+  ) {}
 
   ngOnInit() {
     this.xlsxEformsFileUploader.onSuccessItem = (item, response) => {
-      this.errors = JSON.parse(response).model.errors;
+      const model = JSON.parse(response).model;
+      if (model) {
+        this.errors = JSON.parse(response).model.errors;
+        this.xlsxEformsFileUploader.clearQueue();
+      }
       if (!this.errors) {
         this.xlsxEformsFileUploader.clearQueue();
-        this.toastrService.success(this.translateService.instant('Import has been finished successfully'));
+        this.toastrService.success(
+          this.translateService.instant('Import has been finished successfully')
+        );
         this.frame.hide();
       } else {
-        this.toastrService.warning(this.translateService.instant('Import has been finished partially'));
+        this.toastrService.warning(
+          this.translateService.instant('Import has been finished partially')
+        );
       }
       this.loaderService.isLoading.next(false);
+      this.xlsxEformsInput.nativeElement.value = '';
     };
     this.xlsxEformsFileUploader.onErrorItem = () => {
       this.xlsxEformsFileUploader.clearQueue();
-      this.toastrService.error(this.translateService.instant('Error while making import'));
+      this.toastrService.error(
+        this.translateService.instant('Error while making import')
+      );
+      this.xlsxEformsInput.nativeElement.value = '';
     };
-    this.xlsxEformsFileUploader.onAfterAddingFile = f => {
+    this.xlsxEformsFileUploader.onAfterAddingFile = (f) => {
       if (this.xlsxEformsFileUploader.queue.length > 1) {
-        this.xlsxEformsFileUploader.removeFromQueue(this.xlsxEformsFileUploader.queue[0]);
+        this.xlsxEformsFileUploader.removeFromQueue(
+          this.xlsxEformsFileUploader.queue[0]
+        );
       }
+      this.errors = [];
     };
   }
 
