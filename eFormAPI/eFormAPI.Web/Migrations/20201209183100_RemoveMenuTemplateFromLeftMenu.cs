fb1578a864c00c64587a9073ca4f481ee924f5a8
Leonid <leonid.atf@gmail.com>
2020-12-09 20:36:43 +0200

    Added migrations

---
+++ eFormAPI/eFormAPI.Web/Migrations/20201209183100_RemoveMenuTemplateFromLeftMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eFormAPI.Web.Migrations
+{
+    public partial class RemoveMenuTemplateFromLeftMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MenuTemplates",
+                keyColumn: "Id",
+                keyValue: 13);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MenuTemplates",
+                columns: new[] { "Id", "CreatedAt", "CreatedByUserId", "DefaultLink", "E2EId", "EformPluginId", "Name", "UpdatedAt", "UpdatedByUserId", "Version", "WorkflowState" },
+                values: new object[] { 13, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "/advanced/navigation-menu", "menu-editor", null, "Menu Editor", null, 0, 0, null });
+        }
+    }
+}
