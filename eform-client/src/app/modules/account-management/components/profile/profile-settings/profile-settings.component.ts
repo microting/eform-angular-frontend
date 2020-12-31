9d980315c462091a636815a304dc3c1b4aa70251
René Schultz Madsen <rm@microting.dk>
2020-12-31 18:11:10 +0000

    Fixing the da-DK, which should be da.

--- eform-client/src/app/modules/account-management/components/profile/profile-settings/profile-settings.component.ts
+++ eform-client/src/app/modules/account-management/components/profile/profile-settings/profile-settings.component.ts
@@ -22,7 +22,7 @@ export class ProfileSettingsComponent implements OnInit {
   }
 
   countries = [
-    {id: 'da-DK', text: 'Denmark'},
+    {id: 'da', text: 'Denmark'},
     {id: 'de-DE', text: 'Germany'},
     {id: 'en-GB', text: 'United Kingdom'},
     {id: 'en-US', text: 'United States'}
