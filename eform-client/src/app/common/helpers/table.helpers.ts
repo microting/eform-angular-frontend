20ac509c732b6e4c59f71d1c96f34f28278a7d85
Gid <mkazli733@gmail.com>
2021-03-03 20:49:40 +0200

    Added new helper functions

---
+++ eform-client/src/app/common/helpers/table.helpers.ts
@@ -0,0 +1,38 @@
+import { PagedEntityRequest, PageSettingsModel } from 'src/app/common/models';
+
+export const updateTableSorting = (
+  sort: string,
+  currentPageSettings: PageSettingsModel
+): PageSettingsModel => {
+  let updatedPageSettings;
+  if (currentPageSettings.sort === sort) {
+    updatedPageSettings = {
+      ...currentPageSettings,
+      isSortDsc: !currentPageSettings.sort,
+    };
+  } else {
+    updatedPageSettings = { ...currentPageSettings, isSortDsc: false };
+    updatedPageSettings = { ...currentPageSettings, sort };
+  }
+  return updatedPageSettings;
+};
+
+export const updateTablePage = (
+  offset: number | null,
+  requestModel: PagedEntityRequest
+): PagedEntityRequest | null => {
+  if (offset || offset === 0) {
+    let updatedRequestModel = { ...requestModel, offset };
+    if (offset === 0) {
+      updatedRequestModel = { ...updatedRequestModel, pageIndex: 0 };
+    } else {
+      updatedRequestModel = {
+        ...updatedRequestModel,
+        pageIndex: Math.floor(offset / requestModel.pageSize),
+      };
+    }
+    return updatedRequestModel;
+  } else {
+    return null;
+  }
+};
