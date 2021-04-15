393aff0b77a3919b39731918e04c55bf8687a1b6
René Schultz Madsen <rm@microting.com>
2021-04-15 08:08:18 +0000

    Adding new compact statusbar.

--- eform-client/src/app/common/modules/eform-shared/components/index.ts
+++ eform-client/src/app/common/modules/eform-shared/components/index.ts
@@ -2,6 +2,7 @@ export * from './eform-pagination/eform-pagination.component';
 export * from './eform-page-size/eform-page-size.component';
 export * from './eform-subheader/eform-subheader.component';
 export * from './status-bar/status-bar.component';
+export * from './status-bar-compact/status-bar-compact.component';
 export * from './date-formatter/date-formatter.component';
 export * from './eform-tree-view-picker/eform-tree-view-picker.component';
 export * from './eform-collapse-toggler/eform-collapse-toggle.component';
