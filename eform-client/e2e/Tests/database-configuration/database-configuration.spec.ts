47a0c49fc704247eebccb12f442e396effb4d6cf
Alex <sahok6264@gmail.com>
2021-06-12 19:07:18 +0300

    Try fix tests

--- eform-client/e2e/Tests/database-configuration/database-configuration.spec.ts
+++ eform-client/e2e/Tests/database-configuration/database-configuration.spec.ts
@@ -7,11 +7,11 @@ const expect = require('chai').expect;
 describe('Database', function () {
   it('should be configured successfully', function () {
     databasePage.open('/');
-    databasePage.saveBtn.waitForDisplayed({timeout: 90000});
-    databasePage.languageDropdown.waitForDisplayed({timeout: 90000});
+    databasePage.saveBtn.waitForDisplayed({ timeout: 90000 });
+    databasePage.languageDropdown.waitForDisplayed({ timeout: 90000 });
     browser.pause(5000);
     // $('#loginBtn').waitForDisplayed({timeout: 90000});
-    databasePage.languageDropdown.waitForDisplayed({timeout: 90000});
+    databasePage.languageDropdown.waitForDisplayed({ timeout: 90000 });
     browser.pause(1000);
     expect(databasePage.firstNameInput.isDisplayed()).equal(true);
     expect(databasePage.lastNameInput.isDisplayed()).equal(true);
@@ -22,10 +22,12 @@ describe('Database', function () {
     expect(databasePage.authenticationType.isDisplayed()).equal(true);
     expect(databasePage.languageDropdown.isDisplayed()).equal(true);
     if (databasePage.saveBtn.isExisting()) {
-      databasePage.configure(DatabaseConfigurationConstants.languageOptions.danish);
+      databasePage.configure(
+        DatabaseConfigurationConstants.languageOptions.danish
+      );
       browser.pause(1000);
       databasePage.save();
-      $('#loginBtn').waitForDisplayed({timeout: 90000});
+      $('#loginBtn').waitForDisplayed({ timeout: 180000 });
       // browser.pause(45000);
       expect(loginPage.loginBtn.isDisplayed()).equal(true);
     }
