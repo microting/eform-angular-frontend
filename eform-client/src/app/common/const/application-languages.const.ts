65cd69c5ce81d61d80544ca9afcaaab2dab6557c
René Schultz Madsen <rm@microting.com>
2021-10-26 06:32:19 +0000

    Adding an extra constant for those plugins who does not implement German translations completely.

--- eform-client/src/app/common/const/application-languages.const.ts
+++ eform-client/src/app/common/const/application-languages.const.ts
@@ -8,3 +8,15 @@ export const applicationLanguages: {
   { id: 3, locale: 'de-DE', text: 'German' },
   // { id: 4, locale: 'uk-UA', text: 'Ukrainian' },
 ];
+
+// This is here for the plugins who does not have a German implantation
+export const applicationLanguages2: {
+  id: number;
+  locale: string;
+  text: string;
+}[] = [
+  { id: 1, locale: 'da', text: 'Danish' },
+  { id: 2, locale: 'en-US', text: 'English' },
+  // { id: 3, locale: 'de-DE', text: 'German' },
+  // { id: 4, locale: 'uk-UA', text: 'Ukrainian' },
+];
