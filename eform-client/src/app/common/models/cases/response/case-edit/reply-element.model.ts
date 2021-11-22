a7485ee5755f7ba40b31aac2c41f737d9a6d35fb
Gid <mkazli733@gmail.com>
2021-11-22 18:51:50 +0200

    Fix done at editable

--- eform-client/src/app/common/models/cases/response/case-edit/reply-element.model.ts
+++ eform-client/src/app/common/models/cases/response/case-edit/reply-element.model.ts
@@ -3,7 +3,6 @@ import { ElementDto } from 'src/app/common/models';
 export class ReplyElementDto {
   id: number;
   label: string;
-  isDoneAtEditable: boolean;
   doneUserEditable: Date;
   elementList: ElementDto[] = [];
 }
