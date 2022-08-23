2af8f3cb311c017772e00a2708c3803d8557ad9a
Alex <sahok6264@gmail.com>
2022-08-23 20:50:49 +0300

    Migrate application settings page

--- eform-client/src/app/modules/application-settings/application-settings.module.ts
+++ eform-client/src/app/modules/application-settings/application-settings.module.ts
@@ -4,26 +4,34 @@ import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { TranslateModule } from '@ngx-translate/core';
 import { FileUploadModule } from 'ng2-file-upload';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { EformSharedModule } from 'src/app/common/modules/eform-shared/eform-shared.module';
 import { ApplicationSettingsRouting } from './application-settings.routing';
 import { AdminSettingsComponent, ConnectionSetupComponent } from './components';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        NgSelectModule,
-        ApplicationSettingsRouting,
-        MDBBootstrapModule,
-        EformSharedModule,
-        FormsModule,
-        TranslateModule,
-        FileUploadModule,
-        FontAwesomeModule,
-        MatCheckboxModule,
-    ],
+  imports: [
+    CommonModule,
+    NgSelectModule,
+    ApplicationSettingsRouting,
+    EformSharedModule,
+    FormsModule,
+    TranslateModule,
+    FileUploadModule,
+    FontAwesomeModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+  ],
   declarations: [AdminSettingsComponent, ConnectionSetupComponent],
 })
 export class ApplicationSettingsModule {}
