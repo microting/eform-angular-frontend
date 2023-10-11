7839d47767ea603738dc27a8545e88d88e5145ab
René Schultz Madsen <rm@microting.dk>
2023-10-11 13:15:37 +0200

    Fixing broken build.

--- eform-client/src/app/common/pipes/auth-image.pipe.ts
+++ eform-client/src/app/common/pipes/auth-image.pipe.ts
@@ -1,7 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthStateService} from 'src/app/common/store';
-import {firstValueFrom} from "rxjs";
+import {firstValueFrom, lastValueFrom} from 'rxjs';
 
 @Pipe({
   name: 'authImage',
@@ -15,8 +15,9 @@ export class AuthImagePipe implements PipeTransform {
   async transform(src: string): Promise<string> {
     const token = this.authStateService.bearerToken;
     const headers = new HttpHeaders({ Authorization: token });
-    const imageBlob = await firstValueFrom(this.http
-      .get(src, { headers, responseType: 'blob' }));
+    const imageBlob = await lastValueFrom(
+      this.http.get(src, { headers, responseType: 'blob' })
+    );
     const reader = new FileReader();
     return new Promise((resolve, reject) => {
       reader.onloadend = () => resolve(reader.result as string);
