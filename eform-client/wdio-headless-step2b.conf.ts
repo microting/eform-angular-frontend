5f0c8072e78806f75028e64d2588788439b92ce5
René Schultz Madsen <rm@microting.dk>
2025-02-15 17:32:26 +0100

    Trying with a screenshot path

--- eform-client/wdio-headless-step2b.conf.ts
+++ eform-client/wdio-headless-step2b.conf.ts
@@ -110,7 +110,7 @@ export const config: Options.Testrunner = {
   bail: 0,
   //
   // Saves a screenshot to a given path if a command fails.
-  //screenshotPath: './errorShots/',
+  screenshotPath: './errorShots/',
   //
   // Set a base URL in order to shorten url command calls. If your `url` parameter starts
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
