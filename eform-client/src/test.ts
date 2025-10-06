ce440f12e4befc246919466fca81354b79290bfc
René Schultz Madsen <rm@microting.dk>
2025-10-06 19:06:56 +0200

    Adding karma for testing.

--- eform-client/src/test.ts
+++ eform-client/src/test.ts
@@ -1,16 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-isChecked';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-isChecked';
-import 'zone.js/dist/fake-async-isChecked';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
+import 'zone.js';
+import 'zone.js/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare let __karma__: any;
@@ -20,10 +11,10 @@ declare let require: any;
 __karma__.loaded = function () {};
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+// getTestBed().initTestEnvironment(
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting()
+// );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
