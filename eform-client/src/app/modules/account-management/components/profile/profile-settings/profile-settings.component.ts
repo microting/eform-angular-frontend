2fef271aca6ea6f08d4a8dab69f767dedeba5c5c
shawon <shawon@shawons-Mac-mini.local>
2025-10-15 19:38:10 +0600

    added form control to profile setings and installed plugins

--- eform-client/src/app/modules/account-management/components/profile/profile-settings/profile-settings.component.ts
+++ eform-client/src/app/modules/account-management/components/profile/profile-settings/profile-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { GoogleAuthInfoModel } from 'src/app/common/models/auth';
 import { UserSettingsModel } from 'src/app/common/models/settings';
 import {
@@ -32,6 +33,7 @@ import * as R from 'ramda';
     standalone: false
 })
 export class ProfileSettingsComponent implements OnInit {
+  private fb = inject(FormBuilder);
   authStateService = inject(AuthStateService);
   private authStore = inject(Store);
   private googleAuthService = inject(GoogleAuthService);
@@ -54,6 +56,14 @@ export class ProfileSettingsComponent implements OnInit {
   activeLanguages: Array<any> = [];
   public selectCurrentUserIsAdmin$ = this.authStore.select(selectCurrentUserIsAdmin);
   private selectBearerToken$ = this.authStore.select(selectBearerToken);
+
+  profileForm: FormGroup = this.fb.group({
+    locale: new FormControl(''),
+    formats: new FormControl(''),
+    timeZone: new FormControl(''),
+    darkTheme: new FormControl(false),
+    isTwoFactorEnabled: new FormControl(false),
+  });
 
   ngOnInit() {
     this.getEnabledLanguages();
@@ -94,12 +104,25 @@ export class ProfileSettingsComponent implements OnInit {
     });
   }
 
-  isTwoFactorEnabledCheckBoxChanged(e) {
-    if (e.target) {
-      this.googleAuthInfoModel.isTwoFactorEnabled = e.target.checked;
-    } else {
-      return;
-    }
+  // isTwoFactorEnabledCheckBoxChanged(e) {
+  //   if (e.target) {
+  //     this.googleAuthInfoModel.isTwoFactorEnabled = e.target.checked;
+  //   } else {
+  //     return;
+  //   }
+  //   this.googleAuthService
+  //     .updateGoogleAuthenticatorInfo(this.googleAuthInfoModel)
+  //     .subscribe((data) => {
+  //       if (data.success) {
+  //         this.authStateService.logout();
+  //       }
+  //     });
+  // }
+
+  toggleTwoFactor(): void {
+    const isEnabled = this.profileForm.get('isTwoFactorEnabled')?.value;
+    this.googleAuthInfoModel.isTwoFactorEnabled = isEnabled;
+
     this.googleAuthService
       .updateGoogleAuthenticatorInfo(this.googleAuthInfoModel)
       .subscribe((data) => {
@@ -117,40 +140,105 @@ export class ProfileSettingsComponent implements OnInit {
     });
   }
 
+  // deleteGoogleAuthenticatorInfo() {
+  //   this.googleAuthService.deleteGoogleAuthenticatorInfo().subscribe((data) => {
+  //     if (data && data.success) {
+  //       this.googleAuthInfoModel.psk = null;
+  //     }
+  //   });
+  // }
+
+  // updateUserProfileSettings() {
+  //
+  //   if (this.profilePictureUploader.queue.length > 0) {
+  //     this.profilePictureUploader.queue[0].upload();
+  //   }
+  //   this.userSettingsService
+  //     .updateUserSettings(this.userSettingsModel)
+  //     .subscribe((data) => {
+  //       this.userSettings.getUserSettings().subscribe((data) => {
+  //         this.userSettingsModel = data.model;
+  //         this.store.dispatch(updateCurrentUserLocaleAndDarkTheme({
+  //           darkTheme: this.userSettingsModel.darkTheme,
+  //           locale: this.userSettingsModel.locale,
+  //           languageId: this.userSettingsModel.languageId
+  //         }));
+  //         this.getUserSettings();
+  //       });
+  //       this.store.dispatch(loadAppMenu());
+  //     });
+  // }
+
   updateUserProfileSettings() {
+    const formValue = this.profileForm.value;
+
+    this.userSettingsModel.locale = formValue.locale;
+    this.userSettingsModel.formats = formValue.formats;
+    this.userSettingsModel.timeZone = formValue.timeZone;
+    this.userSettingsModel.darkTheme = formValue.darkTheme;
 
     if (this.profilePictureUploader.queue.length > 0) {
       this.profilePictureUploader.queue[0].upload();
     }
-    this.userSettingsService
-      .updateUserSettings(this.userSettingsModel)
-      .subscribe((data) => {
-        this.userSettings.getUserSettings().subscribe((data) => {
-          this.userSettingsModel = data.model;
-          this.store.dispatch(updateCurrentUserLocaleAndDarkTheme({
+
+    this.userSettingsService.updateUserSettings(this.userSettingsModel).subscribe((data) => {
+      this.userSettings.getUserSettings().subscribe((data) => {
+        this.userSettingsModel = data.model;
+        this.store.dispatch(
+          updateCurrentUserLocaleAndDarkTheme({
             darkTheme: this.userSettingsModel.darkTheme,
             locale: this.userSettingsModel.locale,
-            languageId: this.userSettingsModel.languageId
-          }));
-          this.getUserSettings();
-        });
-        this.store.dispatch(loadAppMenu());
+            languageId: this.userSettingsModel.languageId,
+          })
+        );
+        this.getUserSettings();
       });
+      this.store.dispatch(loadAppMenu());
+    });
   }
+
+  // getEnabledLanguages() {
+  //   this.getLanguagesSub$ = this.appSettingsStateService.getLanguages()
+  //     .pipe(tap(data => {
+  //       if (data && data.success && data.model) {
+  //         this.appLanguages = data.model;
+  //         this.activeLanguages = this.appLanguages.languages.filter((x) => x.isActive);
+  //         this.getTimeZones();
+  //         this.getGoogleAuthenticatorInfo();
+  //         this.getUserSettings();
+  //       }
+  //     }))
+  //     .subscribe();
+  // }
 
   getEnabledLanguages() {
     this.getLanguagesSub$ = this.appSettingsStateService.getLanguages()
-      .pipe(tap(data => {
-        if (data && data.success && data.model) {
-          this.appLanguages = data.model;
-          this.activeLanguages = this.appLanguages.languages.filter((x) => x.isActive);
-          this.getTimeZones();
-          this.getGoogleAuthenticatorInfo();
-          this.getUserSettings();
-        }
-      }))
+      .pipe(
+        tap((data) => {
+          if (data && data.success && data.model) {
+            this.appLanguages = data.model;
+            this.activeLanguages = this.appLanguages.languages.filter((x) => x.isActive);
+            this.getTimeZones();
+            this.getGoogleAuthenticatorInfo();
+
+            // Fetch user settings after languages are available
+            this.userSettingsService.getUserSettings().subscribe((data) => {
+              this.userSettingsModel = data.model;
+
+              this.profileForm.patchValue({
+                locale: this.userSettingsModel.locale,
+                formats: this.userSettingsModel.formats,
+                timeZone: this.userSettingsModel.timeZone,
+                darkTheme: this.userSettingsModel.darkTheme,
+                isTwoFactorEnabled: this.googleAuthInfoModel.isTwoFactorEnabled,
+              });
+            });
+          }
+        })
+      )
       .subscribe();
   }
+
 
   get languages() {
     return this.appLanguages.languages.filter((x) => x.isActive);
--- eform-client/src/app/modules/plugins-management/components/installed/installed-plugin-permissions/installed-plugin-permissions.component.ts
+++ eform-client/src/app/modules/plugins-management/components/installed/installed-plugin-permissions/installed-plugin-permissions.component.ts
@@ -1,51 +1,85 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
 import {
   PluginGroupPermissionsListModel,
   PluginGroupPermissionsUpdateModel,
   SecurityGroupModel,
 } from 'src/app/common/models';
+import {FormBuilder, FormGroup, FormControl} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {PluginPermissionsService} from 'src/app/common/services';
 
 @Component({
-    selector: 'app-installed-plugin-permissions',
-    templateUrl: './installed-plugin-permissions.component.html',
-    styleUrls: ['./installed-plugin-permissions.component.scss'],
-    standalone: false
+  selector: 'app-installed-plugin-permissions',
+  templateUrl: './installed-plugin-permissions.component.html',
+  styleUrls: ['./installed-plugin-permissions.component.scss'],
+  standalone: false,
 })
+
 export class InstalledPluginPermissionsComponent implements OnInit {
   private pluginPermissionsService = inject(PluginPermissionsService);
+  private fb = inject(FormBuilder);
   dialogRef = inject<MatDialogRef<InstalledPluginPermissionsComponent>>(MatDialogRef);
 
   securityGroups: SecurityGroupModel[] = [];
   pluginGroupPermissions: PluginGroupPermissionsListModel[] = [];
   pluginId: number;
+  permissionsForm: FormGroup = this.fb.group({});
 
   constructor() {
     const model = inject<{
-    pluginPermissions: PluginGroupPermissionsUpdateModel;
-    securityGroups: SecurityGroupModel[];
-}>(MAT_DIALOG_DATA);
+      pluginPermissions: PluginGroupPermissionsUpdateModel;
+      securityGroups: SecurityGroupModel[];
+    }>(MAT_DIALOG_DATA);
 
     this.pluginId = model.pluginPermissions.pluginId;
     this.pluginGroupPermissions = model.pluginPermissions.groupPermissions;
     this.securityGroups = model.securityGroups;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  private initializeForm() {
+    const controls: { [key: string]: FormControl } = {};
+
+    this.pluginGroupPermissions.forEach(group => {
+      group.permissions.forEach(permission => {
+        const controlName = this.getPermissionControlName(group.groupId, permission.permissionId);
+        controls[controlName] = new FormControl(permission.isEnabled);
+      });
+    });
+
+    this.permissionsForm = this.fb.group(controls);
+  }
+
+  getPermissionControlName(groupId: number, permissionId: number): string {
+    return `perm_${groupId}_${permissionId}`;
+  }
 
   hide(result = false) {
     this.dialogRef.close(result);
   }
 
   getSecurityGroupName(id: number) {
-    const group = this.securityGroups.find((g) => g.id === id);
+    const group = this.securityGroups.find(g => g.id === id);
     return group ? group.groupName : '';
   }
 
   updatePluginPermissions() {
+
+    this.pluginGroupPermissions.forEach(group => {
+      group.permissions.forEach(permission => {
+        const controlName = this.getPermissionControlName(group.groupId, permission.permissionId);
+        permission.isEnabled = this.permissionsForm.get(controlName)?.value ?? false;
+      });
+    });
+
     this.pluginPermissionsService
-      .updatePluginGroupPermissions({pluginId: this.pluginId, groupPermissions: this.pluginGroupPermissions})
+      .updatePluginGroupPermissions({
+        pluginId: this.pluginId,
+        groupPermissions: this.pluginGroupPermissions,
+      })
       .subscribe((data) => {
         if (data && data.success) {
           this.hide(true);
