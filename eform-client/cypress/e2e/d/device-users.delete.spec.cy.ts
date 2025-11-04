c6837cab6d6e6d2914495b05e36d7abc4ac641fd
René Schultz Madsen <rm@microting.dk>
2025-11-04 11:38:53 +0100

    Fixing d cypress tests.

--- eform-client/cypress/e2e/d/device-users.delete.spec.cy.ts
+++ eform-client/cypress/e2e/d/device-users.delete.spec.cy.ts
@@ -4,12 +4,15 @@ import { Guid } from 'guid-typescript';
 import { expect } from 'chai';
 
 describe('Device users page - Delete device user', function () {
-  before(() => {
+  beforeEach(() => {
     cy.visit('http://localhost:4200');
     loginPage.login();
     cy.intercept('POST', '**/api/device-users/index').as('loadDeviceUsers');
     deviceUsersPage.Navbar.goToDeviceUsersPage();
     cy.wait('@loadDeviceUsers', { timeout: 30000 });
+  });
+
+  it('should not delete device user if cancel was clicked', () => {
 
     // Create a test user to delete
     const firstName = Guid.create().toString();
@@ -17,17 +20,14 @@ describe('Device users page - Delete device user', function () {
     cy.get('#newDeviceUserBtn', { timeout: 10000 }).should('be.visible').click();
     cy.get('#firstName').should('be.visible').type(firstName);
     cy.get('#lastName').should('be.visible').type(lastName);
-    
-    cy.intercept('POST', '**/api/device-users/create').as('createUser');
+
+    cy.intercept('PUT', '**/api/device-users/create').as('createUser');
     cy.get('#saveCreateBtn').should('be.visible').click();
     cy.wait('@createUser', { timeout: 30000 });
     cy.get('#newDeviceUserBtn').should('be.visible');
-  });
-
-  it('should not delete device user if cancel was clicked', () => {
     // Ensure table is visible before counting rows
     cy.get('tbody > tr', { timeout: 10000 }).should('have.length.gt', 0);
-    
+
     deviceUsersPage.rowNum().then((rowNumBeforeDelete) => {
       cy.get('#deviceUserId').should('be.visible');
 
@@ -35,11 +35,6 @@ describe('Device users page - Delete device user', function () {
       cy.get('#deleteDeviceUserBtn').last().should('be.visible').click();
       cy.get('#cancelDeleteBtn').should('be.visible').click();
 
-      // Navigate back to device users page
-      cy.intercept('POST', '**/api/device-users/index').as('reloadList');
-      deviceUsersPage.Navbar.goToDeviceUsersPage();
-      cy.wait('@reloadList', { timeout: 30000 });
-      
       // Ensure table is visible before counting rows
       cy.get('tbody > tr', { timeout: 10000 }).should('have.length.gt', 0);
 
@@ -51,38 +46,21 @@ describe('Device users page - Delete device user', function () {
   });
 
   it('should delete device user successfully', () => {
-    cy.intercept('POST', '**/api/device-users/index').as('loadDeviceUsers1');
-    deviceUsersPage.Navbar.goToDeviceUsersPage();
-    cy.wait('@loadDeviceUsers1', { timeout: 30000 });
-    
-    // Ensure table is visible before counting rows
     cy.get('tbody > tr', { timeout: 10000 }).should('have.length.gt', 0);
 
     deviceUsersPage.rowNum().then((rowNumBeforeDelete) => {
       cy.get('#deviceUserId').should('be.visible');
 
       // Click delete button on last row
-      cy.intercept('POST', '**/api/device-users/delete').as('deleteUser');
+      cy.intercept('DELETE', '**/api/device-users/delete/*').as('deleteUser');
       cy.intercept('POST', '**/api/device-users/index').as('reloadAfterDelete');
       cy.get('#deleteDeviceUserBtn').last().should('be.visible').click();
       cy.get('#saveDeleteBtn').should('be.visible').click();
       cy.wait('@deleteUser', { timeout: 30000 });
       cy.wait('@reloadAfterDelete', { timeout: 30000 });
 
-      // Navigate back to device users page
-      cy.intercept('POST', '**/api/device-users/index').as('loadDeviceUsers2');
-      deviceUsersPage.Navbar.goToDeviceUsersPage();
-      cy.wait('@loadDeviceUsers2', { timeout: 30000 });
-      
-      // Ensure table is visible before counting rows
-      cy.get('tbody > tr', { timeout: 10000 }).should('have.length.gt', 0);
+      cy.get('tbody > tr', { timeout: 10000 }).should('have.length', 0);
 
-      // Verify count decreased
-      deviceUsersPage.rowNum().then((rowNumAfterDelete) => {
-        expect(rowNumBeforeDelete, 'User deleted incorrectly').to.equal(
-          rowNumAfterDelete + 1
-        );
-      });
     });
   });
 });
