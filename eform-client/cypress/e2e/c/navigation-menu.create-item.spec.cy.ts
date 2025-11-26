8daebb5bbb5d3d1688c7ac0fd7f74e1f1755a205
René Schultz Madsen <rm@microting.dk>
2025-11-26 11:13:26 +0100

    Commenting the c cypress tests for now.

--- eform-client/cypress/e2e/c/navigation-menu.create-item.spec.cy.ts
+++ eform-client/cypress/e2e/c/navigation-menu.create-item.spec.cy.ts
@@ -10,170 +10,170 @@ describe('Navigation menu - Create item', function () {
     cy.wait('@loadMenu', { timeout: 30000 });
   });
 
-  it('element must be moved from templates to list', () => {
-    // Get initial count
-    navigationMenuPage.getMenuItems().its('length').then(initialCount => {
-      navigationMenuPage.collapseTemplates(0);
-      navigationMenuPage.createMenuItemFromTemplate(0);
-      
-      // Wait for DOM update after drag-and-drop (matching WDIO test behavior)
-      cy.wait(500);
-
-      // Verify count increased
-      navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
-      
-      // Intercept save operation
-      cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
-      navigationMenuPage.clickSaveMenuBtn();
-      cy.wait('@saveMenu', { timeout: 30000 });
-      
-      navigationMenuPage.openEditMenuItem(0);
-      
-      // Verify link field
-      cy.get('#editLinkInput').should('have.value', '/');
-      
-      // Verify translations
-      cy.get('#editItemTranslation0_0_0').should('have.value', 'My eForms');
-      cy.get('#editItemTranslation0_0_1').should('have.value', 'Mine eForms');
-      cy.get('#editItemTranslation0_0_2').should('have.value', 'Meine eForms');
-      
-      // Intercept edit save operation
-      cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
-      navigationMenuPage.editItemSave();
-      cy.wait('@saveMenuEdit', { timeout: 30000 });
-      
-      navigationMenuPage.collapseTemplates(0);
-    });
-  });
-
-  it('element must be created from custom link', () => {
-    const customLink = {
-      securityGroups: [],
-      link: 'test0',
-      translations: ['test1', 'test2', 'test3']
-    };
-
-    navigationMenuPage.getMenuItems().its('length').then(initialCount => {
-      navigationMenuPage.collapseTemplates(1);
-      navigationMenuPage.createCustomLink(customLink);
-      
-      // Wait for DOM update after creating custom link (matching WDIO test behavior)
-      cy.wait(1000);
-
-      // Verify count increased
-      navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
-      
-      // Intercept save operation
-      cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
-      navigationMenuPage.clickSaveMenuBtn();
-      cy.wait('@saveMenu', { timeout: 30000 });
-
-      // Open last item for editing
-      navigationMenuPage.getMenuItems().its('length').then(count => {
-        navigationMenuPage.openEditMenuItem(count - 1);
-
-        // Verify link
-        cy.get('#editLinkInput').should('have.value', customLink.link);
-
-        // Verify translations
-        customLink.translations.forEach((translation, i) => {
-          navigationMenuPage.getMenuItems().its('length').then(itemCount => {
-            cy.get(`#editItemTranslation${itemCount - 1}_0_${i}`).should('have.value', translation);
-          });
-        });
-
-        // Intercept edit save operation
-        cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
-        navigationMenuPage.editItemSave();
-        cy.wait('@saveMenuEdit', { timeout: 30000 });
-      });
-    });
-  });
-
-  it('element must be created from custom dropdown', () => {
-    const dropdown = {
-      securityGroups: [],
-      translations: ['test1', 'test2', 'test3']
-    };
-
-    navigationMenuPage.getMenuItems().its('length').then(initialCount => {
-      navigationMenuPage.collapseTemplates(1);
-      navigationMenuPage.createCustomDropdown(dropdown);
-      
-      // Wait for DOM update after creating dropdown (matching WDIO test behavior)
-      cy.wait(1500);
-
-      // Verify count increased
-      navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
-      
-      // Intercept save operation
-      cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
-      navigationMenuPage.clickSaveMenuBtn();
-      cy.wait('@saveMenu', { timeout: 30000 });
-
-      // Open last item for editing
-      navigationMenuPage.getMenuItems().its('length').then(count => {
-        navigationMenuPage.openEditMenuItem(count - 1);
-
-        // Verify translations
-        dropdown.translations.forEach((translation, i) => {
-          navigationMenuPage.getMenuItems().its('length').then(itemCount => {
-            cy.get(`#editItemTranslation${itemCount - 1}_0_${i}`).should('have.value', translation);
-          });
-        });
-
-        // Intercept edit save operation
-        cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
-        navigationMenuPage.editItemSave();
-        cy.wait('@saveMenuEdit', { timeout: 30000 });
-      });
-    });
-  });
-
-  it('element must be created from custom dropdown with security group', () => {
-    const dropdown = {
-      securityGroups: ['eForm admins'],
-      translations: ['test1', 'test2', 'test3']
-    };
-
-    navigationMenuPage.getMenuItems().its('length').then(initialCount => {
-      navigationMenuPage.collapseTemplates(1);
-      navigationMenuPage.createCustomDropdown(dropdown);
-      
-      // Wait for DOM update after creating dropdown (matching WDIO test behavior)
-      cy.wait(1500);
-
-      // Verify count increased
-      navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
-      
-      // Intercept save operation
-      cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
-      navigationMenuPage.clickSaveMenuBtn();
-      cy.wait('@saveMenu', { timeout: 30000 });
-
-      // Open last item for editing
-      navigationMenuPage.getMenuItems().its('length').then(count => {
-        navigationMenuPage.openEditMenuItem(count - 1);
-
-        // Verify security groups
-        dropdown.securityGroups.forEach((group, i) => {
-          navigationMenuPage.getSecurityGroupsValue().eq(i).should('have.text', group);
-        });
-
-        // Verify translations
-        dropdown.translations.forEach((translation, i) => {
-          navigationMenuPage.getMenuItems().its('length').then(itemCount => {
-            cy.get(`#editItemTranslation${itemCount - 1}_0_${i}`).should('have.value', translation);
-          });
-        });
-
-        // Intercept edit save operation
-        cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
-        navigationMenuPage.editItemSave();
-        cy.wait('@saveMenuEdit', { timeout: 30000 });
-      });
-    });
-  });
+  // it('element must be moved from templates to list', () => {
+  //   // Get initial count
+  //   navigationMenuPage.getMenuItems().its('length').then(initialCount => {
+  //     navigationMenuPage.collapseTemplates(0);
+  //     navigationMenuPage.createMenuItemFromTemplate(0);
+  //
+  //     // Wait for DOM update after drag-and-drop (matching WDIO test behavior)
+  //     cy.wait(500);
+  //
+  //     // Verify count increased
+  //     navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
+  //
+  //     // Intercept save operation
+  //     cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
+  //     navigationMenuPage.clickSaveMenuBtn();
+  //     cy.wait('@saveMenu', { timeout: 30000 });
+  //
+  //     navigationMenuPage.openEditMenuItem(0);
+  //
+  //     // Verify link field
+  //     cy.get('#editLinkInput').should('have.value', '/');
+  //
+  //     // Verify translations
+  //     cy.get('#editItemTranslation0_0_0').should('have.value', 'My eForms');
+  //     cy.get('#editItemTranslation0_0_1').should('have.value', 'Mine eForms');
+  //     cy.get('#editItemTranslation0_0_2').should('have.value', 'Meine eForms');
+  //
+  //     // Intercept edit save operation
+  //     cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
+  //     navigationMenuPage.editItemSave();
+  //     cy.wait('@saveMenuEdit', { timeout: 30000 });
+  //
+  //     navigationMenuPage.collapseTemplates(0);
+  //   });
+  // });
+  //
+  // it('element must be created from custom link', () => {
+  //   const customLink = {
+  //     securityGroups: [],
+  //     link: 'test0',
+  //     translations: ['test1', 'test2', 'test3']
+  //   };
+  //
+  //   navigationMenuPage.getMenuItems().its('length').then(initialCount => {
+  //     navigationMenuPage.collapseTemplates(1);
+  //     navigationMenuPage.createCustomLink(customLink);
+  //
+  //     // Wait for DOM update after creating custom link (matching WDIO test behavior)
+  //     cy.wait(1000);
+  //
+  //     // Verify count increased
+  //     navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
+  //
+  //     // Intercept save operation
+  //     cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
+  //     navigationMenuPage.clickSaveMenuBtn();
+  //     cy.wait('@saveMenu', { timeout: 30000 });
+  //
+  //     // Open last item for editing
+  //     navigationMenuPage.getMenuItems().its('length').then(count => {
+  //       navigationMenuPage.openEditMenuItem(count - 1);
+  //
+  //       // Verify link
+  //       cy.get('#editLinkInput').should('have.value', customLink.link);
+  //
+  //       // Verify translations
+  //       customLink.translations.forEach((translation, i) => {
+  //         navigationMenuPage.getMenuItems().its('length').then(itemCount => {
+  //           cy.get(`#editItemTranslation${itemCount - 1}_0_${i}`).should('have.value', translation);
+  //         });
+  //       });
+  //
+  //       // Intercept edit save operation
+  //       cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
+  //       navigationMenuPage.editItemSave();
+  //       cy.wait('@saveMenuEdit', { timeout: 30000 });
+  //     });
+  //   });
+  // });
+  //
+  // it('element must be created from custom dropdown', () => {
+  //   const dropdown = {
+  //     securityGroups: [],
+  //     translations: ['test1', 'test2', 'test3']
+  //   };
+  //
+  //   navigationMenuPage.getMenuItems().its('length').then(initialCount => {
+  //     navigationMenuPage.collapseTemplates(1);
+  //     navigationMenuPage.createCustomDropdown(dropdown);
+  //
+  //     // Wait for DOM update after creating dropdown (matching WDIO test behavior)
+  //     cy.wait(1500);
+  //
+  //     // Verify count increased
+  //     navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
+  //
+  //     // Intercept save operation
+  //     cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
+  //     navigationMenuPage.clickSaveMenuBtn();
+  //     cy.wait('@saveMenu', { timeout: 30000 });
+  //
+  //     // Open last item for editing
+  //     navigationMenuPage.getMenuItems().its('length').then(count => {
+  //       navigationMenuPage.openEditMenuItem(count - 1);
+  //
+  //       // Verify translations
+  //       dropdown.translations.forEach((translation, i) => {
+  //         navigationMenuPage.getMenuItems().its('length').then(itemCount => {
+  //           cy.get(`#editItemTranslation${itemCount - 1}_0_${i}`).should('have.value', translation);
+  //         });
+  //       });
+  //
+  //       // Intercept edit save operation
+  //       cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
+  //       navigationMenuPage.editItemSave();
+  //       cy.wait('@saveMenuEdit', { timeout: 30000 });
+  //     });
+  //   });
+  // });
+  //
+  // it('element must be created from custom dropdown with security group', () => {
+  //   const dropdown = {
+  //     securityGroups: ['eForm admins'],
+  //     translations: ['test1', 'test2', 'test3']
+  //   };
+  //
+  //   navigationMenuPage.getMenuItems().its('length').then(initialCount => {
+  //     navigationMenuPage.collapseTemplates(1);
+  //     navigationMenuPage.createCustomDropdown(dropdown);
+  //
+  //     // Wait for DOM update after creating dropdown (matching WDIO test behavior)
+  //     cy.wait(1500);
+  //
+  //     // Verify count increased
+  //     navigationMenuPage.getMenuItems().should('have.length', initialCount + 1);
+  //
+  //     // Intercept save operation
+  //     cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
+  //     navigationMenuPage.clickSaveMenuBtn();
+  //     cy.wait('@saveMenu', { timeout: 30000 });
+  //
+  //     // Open last item for editing
+  //     navigationMenuPage.getMenuItems().its('length').then(count => {
+  //       navigationMenuPage.openEditMenuItem(count - 1);
+  //
+  //       // Verify security groups
+  //       dropdown.securityGroups.forEach((group, i) => {
+  //         navigationMenuPage.getSecurityGroupsValue().eq(i).should('have.text', group);
+  //       });
+  //
+  //       // Verify translations
+  //       dropdown.translations.forEach((translation, i) => {
+  //         navigationMenuPage.getMenuItems().its('length').then(itemCount => {
+  //           cy.get(`#editItemTranslation${itemCount - 1}_0_${i}`).should('have.value', translation);
+  //         });
+  //       });
+  //
+  //       // Intercept edit save operation
+  //       cy.intercept('POST', '**/api/navigation-menu').as('saveMenuEdit');
+  //       navigationMenuPage.editItemSave();
+  //       cy.wait('@saveMenuEdit', { timeout: 30000 });
+  //     });
+  //   });
+  // });
 
   afterEach(() => {
     cy.intercept('POST', '**/api/navigation-menu/reset').as('resetMenu');
--- eform-client/cypress/e2e/c/navigation-menu.delete-item.spec.cy.ts
+++ eform-client/cypress/e2e/c/navigation-menu.delete-item.spec.cy.ts
@@ -10,78 +10,78 @@ describe('Navigation menu - Delete item', function () {
     cy.wait('@loadMenu', { timeout: 30000 });
   });
 
-  it('element must be created from custom dropdown which elements and create templates elements', () => {
-    const dropdown = {
-      securityGroups: [],
-      translations: ['test1', 'test', 'test3']
-    };
-
-    navigationMenuPage.getMenuItems().its('length').then(initialCount => {
-      navigationMenuPage.collapseTemplates(1);
-      
-      navigationMenuPage.createCustomDropdown(dropdown);
-
-      // Create 2 items from templates menu
-      navigationMenuPage.collapseTemplates(0);
-      navigationMenuPage.createMenuItemFromTemplate(2);
-      navigationMenuPage.createMenuItemFromTemplate(3);
-      navigationMenuPage.collapseTemplates(0);
-
-      // Verify 3 elements were created (1 dropdown + 2 template items)
-      navigationMenuPage.getMenuItems().should('have.length', initialCount + 3);
-
-      // Drag template items into dropdown
-      navigationMenuPage.getMenuItems().its('length').then(currentCount => {
-        navigationMenuPage.collapseMenuItemDropdown(currentCount - 1);
-        navigationMenuPage.dragTemplateOnElementInCreatedDropdown(1, currentCount - 1);
-        navigationMenuPage.dragTemplateOnElementInCreatedDropdown(2, currentCount - 1);
-        navigationMenuPage.dragTemplateOnElementInCreatedDropdown(3, currentCount - 1);
-
-        // Verify 3 items in dropdown
-        navigationMenuPage.getDropdownBodyChilds(currentCount - 1).should('have.length', 3);
-
-        // Save menu
-        cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
-        navigationMenuPage.clickSaveMenuBtn();
-        cy.wait('@saveMenu', { timeout: 30000 });
-      });
-    });
-  });
-
-  it('should before deleted items from custom dropdown and items menu', () => {
-    navigationMenuPage.getMenuItems().its('length').then(menuItemsCount => {
-      // Get count of elements in dropdown
-      navigationMenuPage.getDropdownBodyChilds(menuItemsCount - 1).its('length').then(dropdownCount => {
-        
-        // Delete 3 elements from dropdown
-        navigationMenuPage.deleteElementFromDropdown(menuItemsCount - 1, 0);
-        navigationMenuPage.deleteElementFromDropdown(menuItemsCount - 1, 0);
-        navigationMenuPage.deleteElementFromDropdown(menuItemsCount - 1, 0);
-        
-        cy.intercept('POST', '**/api/navigation-menu').as('saveMenu1');
-        navigationMenuPage.clickSaveMenuBtn();
-        cy.wait('@saveMenu1', { timeout: 30000 });
-
-        // Verify dropdown is empty
-        navigationMenuPage.getDropdownBodyChilds(menuItemsCount - 1).should('have.length', dropdownCount - 3);
-
-        // Delete menu items
-        navigationMenuPage.deleteElementFromMenuItems(0);
-        navigationMenuPage.deleteElementFromMenuItems(0); // delete 2 template elements
-        
-        navigationMenuPage.getMenuItems().its('length').then(currentCount => {
-          navigationMenuPage.deleteElementFromMenuItems(currentCount - 1); // delete created dropdown
-          
-          cy.intercept('POST', '**/api/navigation-menu').as('saveMenu2');
-          navigationMenuPage.clickSaveMenuBtn();
-          cy.wait('@saveMenu2', { timeout: 30000 });
-
-          // Verify items were deleted
-          navigationMenuPage.getMenuItems().should('have.length', menuItemsCount - 3);
-        });
-      });
-    });
-  });
+  // it('element must be created from custom dropdown which elements and create templates elements', () => {
+  //   const dropdown = {
+  //     securityGroups: [],
+  //     translations: ['test1', 'test', 'test3']
+  //   };
+  //
+  //   navigationMenuPage.getMenuItems().its('length').then(initialCount => {
+  //     navigationMenuPage.collapseTemplates(1);
+  //
+  //     navigationMenuPage.createCustomDropdown(dropdown);
+  //
+  //     // Create 2 items from templates menu
+  //     navigationMenuPage.collapseTemplates(0);
+  //     navigationMenuPage.createMenuItemFromTemplate(2);
+  //     navigationMenuPage.createMenuItemFromTemplate(3);
+  //     navigationMenuPage.collapseTemplates(0);
+  //
+  //     // Verify 3 elements were created (1 dropdown + 2 template items)
+  //     navigationMenuPage.getMenuItems().should('have.length', initialCount + 3);
+  //
+  //     // Drag template items into dropdown
+  //     navigationMenuPage.getMenuItems().its('length').then(currentCount => {
+  //       navigationMenuPage.collapseMenuItemDropdown(currentCount - 1);
+  //       navigationMenuPage.dragTemplateOnElementInCreatedDropdown(1, currentCount - 1);
+  //       navigationMenuPage.dragTemplateOnElementInCreatedDropdown(2, currentCount - 1);
+  //       navigationMenuPage.dragTemplateOnElementInCreatedDropdown(3, currentCount - 1);
+  //
+  //       // Verify 3 items in dropdown
+  //       navigationMenuPage.getDropdownBodyChilds(currentCount - 1).should('have.length', 3);
+  //
+  //       // Save menu
+  //       cy.intercept('POST', '**/api/navigation-menu').as('saveMenu');
+  //       navigationMenuPage.clickSaveMenuBtn();
+  //       cy.wait('@saveMenu', { timeout: 30000 });
+  //     });
+  //   });
+  // });
+  //
+  // it('should before deleted items from custom dropdown and items menu', () => {
+  //   navigationMenuPage.getMenuItems().its('length').then(menuItemsCount => {
+  //     // Get count of elements in dropdown
+  //     navigationMenuPage.getDropdownBodyChilds(menuItemsCount - 1).its('length').then(dropdownCount => {
+  //
+  //       // Delete 3 elements from dropdown
+  //       navigationMenuPage.deleteElementFromDropdown(menuItemsCount - 1, 0);
+  //       navigationMenuPage.deleteElementFromDropdown(menuItemsCount - 1, 0);
+  //       navigationMenuPage.deleteElementFromDropdown(menuItemsCount - 1, 0);
+  //
+  //       cy.intercept('POST', '**/api/navigation-menu').as('saveMenu1');
+  //       navigationMenuPage.clickSaveMenuBtn();
+  //       cy.wait('@saveMenu1', { timeout: 30000 });
+  //
+  //       // Verify dropdown is empty
+  //       navigationMenuPage.getDropdownBodyChilds(menuItemsCount - 1).should('have.length', dropdownCount - 3);
+  //
+  //       // Delete menu items
+  //       navigationMenuPage.deleteElementFromMenuItems(0);
+  //       navigationMenuPage.deleteElementFromMenuItems(0); // delete 2 template elements
+  //
+  //       navigationMenuPage.getMenuItems().its('length').then(currentCount => {
+  //         navigationMenuPage.deleteElementFromMenuItems(currentCount - 1); // delete created dropdown
+  //
+  //         cy.intercept('POST', '**/api/navigation-menu').as('saveMenu2');
+  //         navigationMenuPage.clickSaveMenuBtn();
+  //         cy.wait('@saveMenu2', { timeout: 30000 });
+  //
+  //         // Verify items were deleted
+  //         navigationMenuPage.getMenuItems().should('have.length', menuItemsCount - 3);
+  //       });
+  //     });
+  //   });
+  // });
 
   afterEach(() => {
     cy.intercept('POST', '**/api/navigation-menu/reset').as('resetMenu');
