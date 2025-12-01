17a12cd92e48603d8e8777d683fff3983df88320
René Schultz Madsen <rm@microting.dk>
2025-12-01 11:18:14 +0100

    Making the code try to lookup by email, as they are the same and update the username accordingly

--- eFormAPI/eFormAPI.Web/Services/UserService.cs
+++ eFormAPI/eFormAPI.Web/Services/UserService.cs
@@ -53,8 +53,23 @@ public class UserService(
 
     public async Task<EformUser> GetByUsernameAsync(string username)
     {
-        return await dbContext.Users.FirstOrDefaultAsync(x => x.UserName == username);
+        var user = await dbContext.Users.FirstOrDefaultAsync(x => x.UserName == username);
+        if (user == null)
+        {
+            user = await dbContext.Users.FirstOrDefaultAsync(x => x.Email == username);
+            if (user != null)
+            {
+                user.UserName = username;
+                await userManager.UpdateAsync(user);
+                return user;
+            }
+
+            throw new Exception("User not found!");
+        }
+
+        return user;
     }
+
 
     public int UserId
     {
