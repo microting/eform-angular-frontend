a158f4fb2cef4fb5b1e103463b97bfd2cf85d0c8
René Schultz Madsen <rm@microting.com>
2026-04-15 11:03:11 +0200

    Revert "fix(calendar): place required * directly after field name in eForm preview"
    This reverts commit ef384bea8995db661723a273f271531bf245dee2.

--- eform-client/cypress/e2e/plugins/time-planning-pn/o/assert-true.spec.cy.ts
+++
@@ -1,6 +0,0 @@
-// Temporarily disabled tests
-// describe('Assert true', () => {
-//   it('should be true', () => {
-//     expect(true).to.be.true;
-//   });
-// });
--- eform-client/e2e/Tests/items-planning-general/assert-true.spec.ts
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-import { $ } from '@wdio/globals';
-// create canary in a coal mine test asserting true
-it('asserts true', () => {
-  // @ts-ignore
-  expect(true).eq(true) // this will pass
-});
